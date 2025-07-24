fe4be84e0710b77456ab52c6e19faa942ab16015
MappingMetabolismofIstanbul <dogatmn@gmail.com>
2025-07-24 12:29:07 +0300

    Update config.js

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ var config = {
     theme: 'light',
     use3dTerrain: false, //set true for enabling 3D maps.
     auto: false,
-    title: 'Taksim Kentsel Tasarım Yarışması  / Taksim Urban Design Competition',
+    title: 'Kentsel Boşluğu Tasarlamak : Taksim Kentsel Tasarım Yarışması  / Designing the Urban Void : Taksim Urban Design Competition',
     subtitle: '',
     byline: 'BAP_Mapping Metabolism of Istanbul_Interactive Mapping / MEF AAP_027',
     footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
